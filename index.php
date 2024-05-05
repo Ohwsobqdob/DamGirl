04b8ea9237d555c655eac2e14e2b8b6950548a57
Ohwsobqdob <166311432+Ohwsobqdob@users.noreply.github.com>
2024-05-05 15:35:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 $dir = $_SERVER['DOCUMENT_ROOT'];
-
+$baseStorage = $_SERVER['DOCUMENT_ROOT'].getenv('RAILWAY_VOLUME_MOUNT_PATH')."/storage";
 // Sort in ascending order - this is default
 $a = scandir($dir);
 
 // Sort in descending order
 $b = scandir($dir,1);
 
-print_r($a);//print_r($b);
+//print_r($a);
+//print_r($b);
+echo $baseStorage;
 ?>
